98bfb601a3536a7b50f70a5971ead90ba81f03c0
CaDucT <nurik39@gmail.com>
2022-08-15 09:11:11 +0200

    Guess the number game

--- GuessTheNumber/guess_the_number.py
+++ GuessTheNumber/guess_the_number.py
@@ -1,27 +1,27 @@
 import random
 
-randomnoe_cislo = random.randint(0, 100)
+randomn_number = random.randint(0, 100)
 
 chances = 7
 
 def guess(chances):
 
     if chances >= 1:
-        vvod_cisla = int(input('Введите число: '))
-        if vvod_cisla != randomnoe_cislo and vvod_cisla > randomnoe_cislo and chances != 1:
-            print('Неправильно, Я загадал меньше. Осталось', chances-1, 'попыток')
+        entered_number = int(input('Enter the number: '))
+        if entered_number != randomn_number and entered_number > randomn_number and chances != 1:
+            print('Wrong. My number is lower than yours.', chances-1, 'tries left')
             chances -= 1
             guess(chances)
-        elif vvod_cisla != randomnoe_cislo and vvod_cisla < randomnoe_cislo and chances != 1:
-            print('Неправильно. Я загадал больше. Осталось', chances-1, 'попыток')
+        elif entered_number != randomn_number and entered_number < randomn_number and chances != 1:
+            print('Wrong. My number is greater than yours.', chances-1, 'tries left')
             chances -= 1
             guess(chances)
-        elif vvod_cisla == randomnoe_cislo:
-            print('Правильно. Я загадал ', randomnoe_cislo)
-        elif vvod_cisla != randomnoe_cislo and vvod_cisla > randomnoe_cislo and chances == 1:
-            print('Ты проиграл. Я загадал', randomnoe_cislo)
-        elif vvod_cisla != randomnoe_cislo and vvod_cisla < randomnoe_cislo and chances == 1:
-            print('Ты проиграл. Я загадал', randomnoe_cislo)
+        elif entered_number == randomn_number:
+            print('You are right. My number is ', randomn_number)
+        elif entered_number != randomn_number and entered_number > randomn_number and chances == 1:
+            print('You lost. My number is', randomn_number)
+        elif entered_number != randomn_number and entered_number < randomn_number and chances == 1:
+            print('You lost. My number is', randomn_number)
 
 
 if __name__ == '__main__':
