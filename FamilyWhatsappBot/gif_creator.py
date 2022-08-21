5d4cb6b67075ba86f3dc24f1f513259f0c598a89
CaDucT <nurik39@gmail.com>
2022-08-21 21:21:18 +0200

    gif creator

--- FamilyWhatsappBot/gif_creator.py
+++ FamilyWhatsappBot/gif_creator.py
@@ -8,7 +8,7 @@ def make_gif(frame_folder):
     frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
     frame_one = frames[0]
     frame_one.save("new.gif", format="GIF", append_images=frames,
-                   save_all=True, duration=2000, loop=0, )
+                   save_all=True, duration=1000, loop=0, )
 
 
 if __name__ == "__main__":
