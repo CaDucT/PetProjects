70a352612d99faa4229a8f41d44cdf7380964814
CaDucT <nurik39@gmail.com>
2022-08-21 18:56:42 +0200

    vk bot, btc, curr, weather only

--- VkBot/vk_bot_final.py
+++ VkBot/vk_bot_final.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import pyautogui
-from PIL import Image
 import time
 
 ser = "C:\\webdrivers\\chromedriver.exe" #implement chromedriver path
