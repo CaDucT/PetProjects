b162da0b729eb2576399995c5677fac61a184ab5
CaDucT <nurik39@gmail.com>
2022-08-21 19:57:23 +0200

    Creates GIF

--- VkBot/gif_creator.py
+++ VkBot/gif_creator.py
@@ -7,7 +7,7 @@ from PIL import Image
 def make_gif(frame_folder):
     frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
     frame_one = frames[0]
-    frame_one.save("google.gif", format="GIF", append_images=frames,
+    frame_one.save("new.gif", format="GIF", append_images=frames,
                    save_all=True, duration=2000, loop=0, )
 
 
